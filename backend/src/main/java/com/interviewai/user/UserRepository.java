e4e779725f07caaf20401e1d0359f65bd50d0c15
michaelwong3049 <michaelwong3049@gmail.com>
2025-01-19 11:17:55 -0500

    rename user class to profile class

--- backend/src/main/java/com/interviewai/user/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.interviewai.user;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-//limport com.interviewai.user.User;
-
-@Repository
-public interface UserRepository extends CrudRepository<User, Long> { }
