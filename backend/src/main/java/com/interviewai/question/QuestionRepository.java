5035290e2da153d7885f88efdf505d731399d41d
michaelwong3049 <michaelwong3049@gmail.com>
2025-01-21 11:06:39 -0500

    remove last bits of backend code before database migration

--- backend/src/main/java/com/interviewai/question/QuestionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.interviewai.question;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuestionRepository extends CrudRepository<Question, String> {
-    
-}
