7b187a62738b2b7923f7a01a0ba8480c5827569a
michaelwong3049 <michaelwong3049@gmail.com>
2025-01-25 13:24:37 -0500

    add CRUD operations for interviewer table

---
+++ backend/interviewai/src/main/java/com/interviewai/interviewer/InterviewerService.java
@@ -0,0 +1,48 @@
+package com.interviewai.interviewer;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class InterviewerService {
+
+    private final InterviewerRepository interviewerRepository;
+
+    @Autowired
+    public InterviewerService(InterviewerRepository interviewerRepository) {
+        this.interviewerRepository = interviewerRepository;
+    }
+
+    public Optional<Interviewer> getInterviewer(Long id) {
+        try {
+            return interviewerRepository.findById(id);
+        } catch (Exception exception){
+            throw exception;
+        }
+    }
+
+    public List<Interviewer> getInterviewers() {
+        return interviewerRepository.findAll();
+    }
+
+    public Interviewer createInterviewer(Interviewer interviewer) throws Exception {
+        try {
+            interviewerRepository.save(interviewer);
+            return interviewer;
+        } catch (Exception exception) {
+            throw new Exception("Error saving interviewer in InterviewerService");
+        }
+    }
+
+    public void deleteInterviewer(Long id) throws Exception {
+        try {
+            interviewerRepository.deleteById(id);
+        } catch (Exception exception) {
+            throw new Exception("Error deleting interviewer in InterviewerService");
+        }
+    }
+
+}
