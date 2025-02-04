f58e29f628064fb1acdb57b3859ae2d47011a95a
michaelwong3049 <michaelwong3049@gmail.com>
2025-02-04 14:28:42 -0500

    fix: remove random accidental import

--- backend/mockr/src/main/java/com/mockr/interview/InterviewController.java
+++ backend/mockr/src/main/java/com/mockr/interview/InterviewController.java
@@ -3,7 +3,6 @@ package com.mockr.interview;
 import java.util.List;
 import java.util.Optional;
 
-import org.aspectj.internal.lang.annotation.ajcDeclareAnnotation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
