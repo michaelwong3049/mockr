96da9887756c4dc1145480b3f25ac354b8e4b136
michaelwong3049 <michaelwong3049@gmail.com>
2025-02-04 16:00:55 -0500

    add functionality to add user to database

--- frontend/src/app/[username]/dashboard/page.tsx
+++ frontend/src/app/[username]/dashboard/page.tsx
@@ -1,10 +1,43 @@
+"use client";
 import Header from '@/components/Header';
+import { useAuth } from '@clerk/nextjs';
 
 export default function Dashboard() {
+  const { userId } = useAuth();
+
+  const createInterviewer = async () => {
+    const data = await fetch("/api/interviewer", {
+      method: "GET",
+      headers: {
+	"Content-Type": "application/json"
+      }
+    })
+    const interviewer = await data.json();
+  
+    if(interviewer.status != 500) {
+      console.log("FOUND INTERVIEWER");
+      console.log(interviewer);
+    }
+
+    else {
+      const data = await fetch("/api/interviewer", {
+	method: "POST",
+	headers: {
+	  "Content-Type": "application/json"
+	}
+      })
+      
+      const interviewer = await data.json();
+      console.log("CREATED INTERVIEWER");
+      console.log(interviewer);
+    }
+  }
+
+  createInterviewer();
+
     return(
         <div>
             <Header/>
-            <Sidebar/>
         </div>
     );
 }
