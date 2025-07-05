bd03c4e21b3f5a2a313f7163beab11f47345e17f
michaelwong3049 <michaelwong3049@gmail.com>
2025-07-04 22:43:54 -0400

    feat: add functionality to run Python code using modal

--- backend/python-backend/sandbox.py
+++ backend/python-backend/sandbox.py
@@ -1,23 +1,46 @@
 import sys
+import logging
 
 import modal
 
-import socketio
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+logger = logging.getLogger(__name__)
+
+# initialize modal 
+app = modal.App.lookup("mockr", create_if_missing=True)
+sb = modal.Sandbox.create(app=app)
+
+# flask setup
+app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+@app.route("/modal", methods=["POST"])
+def getCode():
+    data = request.get_json()
+
+    if data is None:
+        return jsonify({"error": "No JSON provided"}), 400
+
+    print(data.get("code"))
+    p = sb.exec("python", "-c", data.get("code"))
+    
+    # this represents the print statements that users use for debugging
+    stdout_print_lines = []
+
+    for line in p.stdout: 
+        print(line)
+        stdout_print_lines.append(line)
+
+    return jsonify({ "receieved": stdout_print_lines })
+    # return jsonify({ "receieved": data })
+
+if __name__ == "__main__":
+    try:
+        app.run(port=5000)
+    except:
+        logger.error("Error running flask app")
 
 
-app = modal.App("example-get-started")
-
-
-@app.function()
-def f(i):
-    if i % 2 == 0:
-        print("hello", i)
-    else:
-        print("world", i, file=sys.stderr)
-
-    return i * i
-
-@app.local_entrypoint()
-def main():
-    print("i is: ", f.local(42))
 
---
+++ frontend/src/app/api/modal/route.ts
@@ -0,0 +1,18 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  const data = await req.json();
+
+  const res = await fetch("http://localhost:5000/modal", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data),
+  })
+
+  const json = await res.json();
+
+  return NextResponse.json({ json });
+}
+
